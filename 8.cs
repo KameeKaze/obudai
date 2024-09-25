5525f858d789ed239d76db29d44af7611175f610
Kamee Kaze <K4m33K4z3@protonmail.com>
2024-09-26 01:17:54 +0200

    Create 8.cs

---
+++ 8.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("N: ");
+        int N = int.Parse(Console.ReadLine());
+
+
+        List<int> lista = new List<int>();
+        lista.Add(N);
+        int K = N;
+
+        Console.WriteLine("\ndebug");
+
+        while (K != 1)
+        {
+            if (K % 2 == 0)
+            {
+                K = K / 2; 
+            }
+            else
+            {
+                K = 3 * K + 1;
+            }
+
+            lista.Add(K);
+
+            Console.WriteLine($"lista {string.Join(", ", lista)}");
+        }
+
+        Console.WriteLine("final");
+        Console.WriteLine(string.Join("-> ", lista));
+    }
+}
