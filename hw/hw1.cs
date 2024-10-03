c7d727e271a240d7ac5b1984824fefcaa1d28205
Kamee Kaze <K4m33K4z3@protonmail.com>
2024-10-03 18:43:08 +0200

    Update hw1.cs

--- hw/hw1.cs
+++ hw/hw1.cs
@@ -2,39 +2,31 @@ namespace HW
 {
     internal class Program
     {
-        public static string Reverse(string s)
-        {
-            char[] charArray = s.ToCharArray();
-            Array.Reverse(charArray);
-            return new string(charArray);
-        }
-
         static void Main(string[] args)
         {
             uint n = uint.Parse(Console.ReadLine());
             string[] words;
             words = new string[n];
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < n; i++)
+            {
                 words[i] = Console.ReadLine();
             }
-
+ 
             string w = Console.ReadLine();
-            char[] letters;
-            for (int i = 0; i < n; i++)
-            {
-                w = w.Replace(words[i], "");
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    if (w.Contains(words[i][j]))
+                    {
+                        w = w.Remove(w.IndexOf(words[i][j]),1);
+                    }
+                }
             }
-            w = Reverse(w); // reverse search
-            for (int i = 0; i < n; i++)
-            {
-                w = w.Replace(words[i], "");
-            }
-            w = Reverse(w); // get back original string
+ 
             Console.WriteLine(w);
-
-
+ 
+ 
         }
     }
-
-
+ 
 }
